8cc996038011930251d4af28d089dee30a529e56
sagarmanghnani <sagarmanghnani@gmail.com>
2020-08-16 20:43:22 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CustomerInfoComponent } from './customer-info/customer-info.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerInfoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
