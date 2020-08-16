8fbd4c01c5d619cbbf3c44aec63c632e819b3546
sagarmanghnani <sagarmanghnani@gmail.com>
2020-08-17 00:14:37 +0530

    added download invoice section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeaderComponent } from './header/header.component';
 import { ThankCustomerComponent } from './thank-customer/thank-customer.component';
 import { AmountPaidComponent } from './amount-paid/amount-paid.component';
 import { InvoiceDetailsComponent } from './invoice-details/invoice-details.component';
+import { DownloadInvoiceComponent } from './download-invoice/download-invoice.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { InvoiceDetailsComponent } from './invoice-details/invoice-details.compo
     HeaderComponent,
     ThankCustomerComponent,
     AmountPaidComponent,
-    InvoiceDetailsComponent
+    InvoiceDetailsComponent,
+    DownloadInvoiceComponent
   ],
   imports: [
     BrowserModule,
