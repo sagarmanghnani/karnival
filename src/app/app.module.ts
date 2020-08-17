8fcde6443a45e4ec1a9e1e227890b24f9dd18c7a
sagarmanghnani <sagarmanghnani@gmail.com>
2020-08-17 22:57:12 +0530

    created green card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ThankCustomerComponent } from './thank-customer/thank-customer.componen
 import { AmountPaidComponent } from './amount-paid/amount-paid.component';
 import { InvoiceDetailsComponent } from './invoice-details/invoice-details.component';
 import { DownloadInvoiceComponent } from './download-invoice/download-invoice.component';
+import { GreenCardComponent } from './green-card/green-card.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { DownloadInvoiceComponent } from './download-invoice/download-invoice.co
     ThankCustomerComponent,
     AmountPaidComponent,
     InvoiceDetailsComponent,
-    DownloadInvoiceComponent
+    DownloadInvoiceComponent,
+    GreenCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/green-card/green-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-green-card',
+  templateUrl: './green-card.component.html',
+  styleUrls: ['./green-card.component.scss']
+})
+export class GreenCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
